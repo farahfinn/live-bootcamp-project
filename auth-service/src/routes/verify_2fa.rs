e803bedaca26b263a23285c752d05db5375b8247
Farah Finn <frhahmoud@yahoo.co.uk>
2025-10-31 20:52:40 -0400

    finished Sprint 4 passing all tests

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,23 +1,60 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use axum_extra::extract::CookieJar;
 use serde::Deserialize;
 
-use crate::domain::{data_store::{LoginAttemptId, TwoFACode}, email::Email};
+use crate::{app_state::AppState, domain::{data_store::{LoginAttemptId, TwoFACode, TwoFACodeStore}, email::Email, error::AuthAPIError}, services::{hashmap_two_fa_code_store::HashmapTwoFACodeStore, hashmap_user_store::HashmapUserStore, hashset_banned_token_store::HashsetBannedTokenStore, mock_email_client::MockEmailClient}, utils::auth::generate_auth_cookie};
 
-pub async fn verify2fa(Json(request): Json<VerifyRequest>) -> impl IntoResponse {
+pub async fn verify2fa(State(state): State<AppState<HashmapUserStore, HashsetBannedTokenStore, HashmapTwoFACodeStore, MockEmailClient>>,
+    jar: CookieJar,
+    Json(request): Json<VerifyRequest>) -> (CookieJar, impl IntoResponse) {
+    // Because the function accepts a VerifyRequest Deserialized Json it will return
+    // 422 if the Json sent with the request is malformed
+    
+    
+    // return 400, bad request if email, login id or 2FA code are not valid 
     let email = match Email::parse(request.email) {
         Ok(e) => e,
-        Err(_) => return StatusCode::BAD_REQUEST.into_response(),
+        Err(_) => return (jar, StatusCode::BAD_REQUEST.into_response()),
     };
     let login_attempt_id = match LoginAttemptId::parse(request.loginattemptid) {
         Ok(id) => id,
-        Err(_) => return StatusCode::BAD_REQUEST.into_response()
+        Err(_) =>  return (jar,StatusCode::BAD_REQUEST.into_response())
     };
 
     let two_fa_code = match TwoFACode::parse(request.two_fa_code) {
         Ok(code) => code,
-        Err(_) => return StatusCode::BAD_REQUEST.into_response(),
+        Err(_) => return (jar, StatusCode::BAD_REQUEST.into_response()),
     };
-    StatusCode::OK.into_response()
+
+    // read the login id and 2FAcode stored when client posts to /login successfully
+    let mut two_fa_code_store = state.two_fa_code_store.write().await;
+    let (store_login_attempt_id, store_two_fa_code) = match two_fa_code_store.get_code(&email).await {
+        Ok(tuple) => tuple,
+        Err(_e) => return (jar, AuthAPIError::IncorrectCredentials.into_response())
+    };
+
+    // if they id and 2FA do not match what is in the store send back a 401
+    if login_attempt_id != store_login_attempt_id || two_fa_code!= store_two_fa_code {
+        return (jar, AuthAPIError::IncorrectCredentials.into_response())
+    };
+
+    // remove the 2FACode from the store
+    let removal_result = two_fa_code_store.remove_code(&email).await;
+    match removal_result {
+        Ok(()) => {},
+        Err(_) => return (jar, AuthAPIError::UnexpectedError.into_response())
+    };
+    // create a cookie
+    let auth_cookie = generate_auth_cookie(email).map_err(|_|AuthAPIError::UnexpectedError);
+    // if cookie has issue generating return an error with the empty jar
+    let auth_cookie= match auth_cookie{
+        Ok(cookie) => cookie,
+        Err(e) => return (jar, e.into_response())
+    };
+    // If no error set the cookie in the jar
+    let updated_jar = jar.add(auth_cookie);
+    // return a 200 OK status 
+    (updated_jar, StatusCode::OK.into_response())
 }
 
 #[derive(Deserialize)]
--- auth-service/tests/api/verify_2fa.rs
+++ auth-service/tests/api/verify_2fa.rs
@@ -1,3 +1,4 @@
+use auth_service::{domain::{data_store::{LoginAttemptId, TwoFACodeStore}, email::Email}, routes::LoginRequest, utils::constants::JWT_COOKIE_NAME};
 use serde_json::json;
 use uuid::Uuid;
 
@@ -73,4 +74,188 @@ async fn should_return_400_if_invalid_input() {
         let response = app.verify2fa(&body).await;
         assert_eq!(response.status().as_u16(), 400);
     }
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+    let app = TestApp::new().await;
+
+    let signup_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+       "requires2FA": true, 
+    });
+
+    let _res = app.signup(&signup_body).await;
+
+    let login_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+    });
+    let _res2 = app.login(&login_body).await;
+    let email = Email::parse("example@email.com".into()).expect("an email should be parsed ");
+    let (login_attempt_id, two_fa_code) = app
+        .two_fa_code_store
+        .read()
+        .await
+        .get_code(&email)
+        .await
+        .expect("login id and 2FA code should be set");
+
+    let correct_login_attempt_id = login_attempt_id.as_ref().to_string();
+    let correct_two_fa_code = two_fa_code.as_ref().to_string();
+    let invalid_uuid = Uuid::new_v4();
+    let invalid_two_fa_code = "123898".to_string();
+    let verify_body = [
+        json!({
+        "email": "example@email.com",
+        "LoginAttemptId": invalid_uuid,
+        "2FACode": correct_two_fa_code,
+    }),
+        json!({
+        "email": "example@email.com",
+        "LoginAttemptId": correct_login_attempt_id,
+        "2FACode": invalid_two_fa_code,
+        })
+    ];
+
+    for body in verify_body {
+        let response = app.verify2fa(&body).await;
+
+        assert_eq!(response.status().as_u16(), 401);
+    }
+}
+
+#[tokio::test]
+async fn should_return_401_if_old_code() {
+    let app = TestApp::new().await;
+
+    let signup_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+       "requires2FA": true, 
+    });
+
+    let _res = app.signup(&signup_body).await;
+
+    let login_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+    });
+    let _res2 = app.login(&login_body).await;
+    let email = Email::parse("example@email.com".into()).expect("an email should be parsed ");
+    let (login_attempt_id, two_fa_code) = app
+        .two_fa_code_store
+        .read()
+        .await
+        .get_code(&email)
+        .await
+        .expect("login id and 2FA code should be set");
+
+    let old_login_attempt_id = login_attempt_id.as_ref().to_string();
+    let old_two_fa_code = two_fa_code.as_ref().to_string();
+
+    // call login again to get new id and 2FA code
+    let _res = app.login(&login_body).await;
+    
+    let verify_body = json!({
+        "email": "example@email.com",
+        "LoginAttemptId": old_login_attempt_id,
+        "2FACode": old_two_fa_code,
+    });
+
+    let response = app.verify2fa(&verify_body).await;
+
+    assert_eq!(response.status().as_u16(), 401);
+    
+}
+
+#[tokio::test]
+async fn should_return_200_if_correct_code() {
+    let app = TestApp::new().await;
+
+    let signup_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+       "requires2FA": true, 
+    });
+
+    let _res = app.signup(&signup_body).await;
+
+    let login_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+    });
+    let _res2 = app.login(&login_body).await;
+    let email = Email::parse("example@email.com".into()).expect("an email should be parsed ");
+    let (login_attempt_id, two_fa_code) = app
+        .two_fa_code_store
+        .read()
+        .await
+        .get_code(&email)
+        .await
+        .expect("login id and 2FA code should be set");
+
+    let correct_login_attempt_id = login_attempt_id.as_ref().to_string();
+    let correct_two_fa_code = two_fa_code.as_ref().to_string();
+    let verify_body = json!({
+        "email": "example@email.com",
+        "LoginAttemptId": correct_login_attempt_id,
+        "2FACode": correct_two_fa_code,
+    });
+
+    let response = app.verify2fa(&verify_body).await;
+
+    assert_eq!(response.status().as_u16(), 200);
+
+    // ensure that an auth cookie is set on login
+    let auth_cookie = response
+        .cookies()
+        .find(|cookie| cookie.name() == JWT_COOKIE_NAME)
+        .expect("a cookie name jwt should be set");
+
+    assert!(!auth_cookie.value().is_empty());
+    
+    
+}
+
+#[tokio::test]
+async fn should_return_401_if_same_code_twice() {
+        let app = TestApp::new().await;
+
+    let signup_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+       "requires2FA": true, 
+    });
+
+    let _res = app.signup(&signup_body).await;
+
+    let login_body = json!({
+       "email": "example@email.com",
+       "password": "Password123",
+    });
+    let _res2 = app.login(&login_body).await;
+    let email = Email::parse("example@email.com".into()).expect("an email should be parsed ");
+    let (login_attempt_id, two_fa_code) = app
+        .two_fa_code_store
+        .read()
+        .await
+        .get_code(&email)
+        .await
+        .expect("login id and 2FA code should be set");
+
+    let correct_login_attempt_id = login_attempt_id.as_ref().to_string();
+    let correct_two_fa_code = two_fa_code.as_ref().to_string();
+    let verify_body = json!({
+        "email": "example@email.com",
+        "LoginAttemptId": correct_login_attempt_id,
+        "2FACode": correct_two_fa_code,
+    });
+
+    let _response = app.verify2fa(&verify_body).await;
+    // sending a request to verify2fa twice should not work
+    let response = app.verify2fa(&verify_body).await;
+
+    assert_eq!(response.status().as_u16(), 401);
 }
